c453555f673505bcd46631571ddec07307e95d92
Victor <mail.baryshnikov@yandex.ru>
2022-08-15 02:26:01 +0400

    formatting

---
+++ 023_Third_Digit/Program.cs
@@ -0,0 +1,28 @@
+﻿// 23. Найти 3-ю цифру числа или сообщить, что её нет
+
+//Вариант1. Случайное число
+Console.Clear();
+Console.WriteLine("Вариант1. Прога сообщает 3-ю цифру случайного числа");
+
+int Number = new Random().Next(1, 5000);
+Console.WriteLine($"Дано число: {Number}");
+
+if (Number / 100 > 0) 
+    Console.WriteLine($"3-я цифра числа {Number} - это {Number / 100 % 10}");
+else 
+    Console.WriteLine($"У числа {Number} нет третьей цифры");
+
+Console.WriteLine();
+
+//Вариант2. Заданное число
+Console.WriteLine("Вариант2. Прога сообщает 3-ю цифру заданного числа");
+
+Console.Write("Введите число - ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+if (A / 100 > 0) 
+    Console.WriteLine($"3-я цифра числа {A} - это {A / 100 % 10}");
+else 
+    Console.WriteLine($"У числа {A} нет третьей цифры");
+
+Console.WriteLine();
---
+++ 024_Weekend_day№/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задание: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+
+//Вариант1. Случайное число
+Console.Clear();
+Console.WriteLine("Вариант1. Случайное число");
+
+int dayNumber = new Random().Next(1, 8);
+string[] Days = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" }; // массив
+string txtResult = "";
+
+if (dayNumber > 5) 
+    txtResult = $", Выходной!!!";
+else 
+    txtResult = $", рабочий день.";
+
+Console.WriteLine($"{dayNumber}-й день недели - это {Days[dayNumber - 1]}{txtResult}");
+Console.WriteLine();
+
+//Вариант12 Случайное число
+Console.WriteLine("Вариант2. Заданное число");
+
+Console.Write("Введите № дня недели - ");
+int A = Convert.ToInt32(Console.ReadLine());
+string[] WeekDays = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
+string txtResult2 = "";
+
+if (A > 5)
+    txtResult2 = $", Выходной!!!"; 
+else 
+    txtResult = $", рабочий день.";
+
+Console.WriteLine();
+
+if (A > 7)
+    Console.WriteLine($"Введен не существующий номер");
+else
+    Console.WriteLine($"{A}-й день недели - это {WeekDays[A - 1]}{txtResult2}");
+
+Console.WriteLine();
