d2da9b523efd10bd2b57d7131efdcefaf991e3d4
Victor <mail.baryshnikov@yandex.ru>
2022-08-15 00:08:39 +0400

    formatting

---
+++ 007_Square/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задание: Вывести квадрат заданного числа
+
+Console.WriteLine($"Это программа для вычисления квадрата числа");
+
+// Вариант 1. Ввод числа с клавиатуры
+Console.Write("Введите число - ");
+int value = Convert.ToInt32(Console.ReadLine());
+int result = value * value;
+
+Console.Write($"Вариант 1. Результат - ");
+Console.WriteLine(result); // или Console.WriteLine($"Итог: {result})
+
+// Вариант 2. Считаем через переменную
+
+int number = new Random().Next(1, 100);
+int result2 = number * number;
+Console.WriteLine($"Вариант 2. Квадрат числа {number} = {result}");
