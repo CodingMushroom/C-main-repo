11a491b195c65f40f79e5d68b7061ceff0deac45
Victor <mail.baryshnikov@yandex.ru>
2022-08-15 00:24:16 +0400

    formatting

---
+++ 008_SquareCheck/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задание: По двум введённым числам проверять является ли первое квадратом второго 
+
+Console.WriteLine("Эта прога выявляет является ли одно число квадратом другого");
+
+Console.Write("Введите число a - ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b - ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a == b * b)
+    Console.WriteLine("ДА");
+else
+    Console.WriteLine("НЕТ");
---
+++ 009_WeekDays/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задание: По введенному номеру дня недели вывести его название
+
+Console.WriteLine("Эта прога выдает день недели согласно номеру");
+
+Console.Write("Введите номер дня в неделе - ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a == 1)
+    Console.WriteLine("Понедельник");
+else if (a == 2)
+    Console.WriteLine("Вторник");
+else if (a == 3)
+    Console.WriteLine("Среда");
+else if (a == 4)
+    Console.WriteLine("Четверг");
+else if (a == 5)
+    Console.WriteLine("Пятница");
+else if (a == 6)
+    Console.WriteLine("Суббота");
+else if (a == 7)
+    Console.WriteLine("Воскресенье");
+
+else
+    Console.WriteLine("Введен неверный номер");
