27a74b6597632360cde51c41717c560409e20aa1
Victor <mail.baryshnikov@yandex.ru>
2022-08-19 16:18:39 +0400

    formatting

---
+++ 026_FOR_example/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задание: использование FOR вместо WHILE
+
+string Method(int count, string text) 
+{
+        string result = String.Empty;
+    for(int i = 0; i < count; i++)   //  1 = счетчик, 2 = условие, 3 = инкримент
+    {
+        result = result + text;
+    }
+    return result;
+}
+string FINAL = Method(5, "easy "); // создаем переменную в которую выводим метод
+Console.WriteLine(FINAL);
+Console.WriteLine();
