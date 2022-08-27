4b643443ebe78936d09e952c668cc1eeec1fb90b
Victor <mail.baryshnikov@yandex.ru>
2022-08-28 01:13:39 +0400

    Formatting

--- 037_CubesTableResult/Program.cs
+++ 037_CubesTableResult/Program.cs
@@ -1,48 +1,44 @@
 ﻿// 37. Напишите метод, который принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-double Method4 (double count)
-{
-    int Cube = 0;
-
-    for(int i = 0; i < count; i++)   //  1 = счетчик, 2 = условие, 3 = инкримент
-    {
-        Cube = Math.Pow(count,3);
-        Console.Write(Cube);
-    }
-     return    
-
-}
-
-
 Console.WriteLine();
-Console.WriteLine("Таблица кубов от 1 до заданного значения N");
-Console.WriteLine("Введите число N: ");
-double N = Convert.ToDouble(Console.ReadLine());
-System.Console.WriteLine();
+Console.Write($"Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
 
-
-int[] GetSqrtTable(int N) // задаю массив
+int[] CubeTable(int N) // метод по вычислению степени
 {
-    int[] SqrtArray = new int[N];
-    int number = 1;
+    int[] Array = new int[N]; // создаем массив с N элементами
+    int Cube = 1;
     for (int i = 0; i < N; i++)
     {
-        SqrtArray[i] = number * number;
-        number++;
+        Array[i] = (int)Math.Pow(Cube,3); // наполняем массив значениями воз-я в степень
+        Cube++;
     }
-    return SqrtArray;
+    return Array;
 }
 
-void PrintTable(int[] Array)
+void PrintTable(int[] Array) // метод вывода на экран таблицы результатов
 {
     int number = 1;
     for (int i = 0; i < Array.Length; i++)
     {
-        System.Console.Write($"{number} ^ 2 = ");
-        System.Console.WriteLine(Array[i]);
+        Console.Write($"{number} ^ 3 = ");
+        Console.WriteLine(Array[i]);
         number++;
     }
 }
 
-int[] SqrtTable = GetSqrtTable(N);
+int[] SqrtTable = CubeTable(N);
 PrintTable(SqrtTable);
+
+
+// Упрощенный вариант решения (без методов):
+//int i = 1;
+//double Cube = 1;
+//string result = String.Empty;
+
+//while (i <= N)
+//{
+//    Cube = Math.Pow(i,3);
+//    Console.Write(Cube.ToString() + " ");
+//    i++;
+//}
