cfacc7e4b5e41e645eb0b57819a156ffbf2f58ac
Victor <mail.baryshnikov@yandex.ru>
2022-09-01 02:31:05 +0400

    Formatting

---
+++ 044_Array_Print/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите метод, который задаёт массив из 8 элементов (диапазон от 1 до 100) и выводит их на экран. (ДЗ)
+
+Console.WriteLine();
+Console.WriteLine("Прога задает массив из 8 элементов и выводит его на экран");
+
+void FillArray(int[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1,100);
+    }
+}
+
+void PrintArray(int[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"|{array[i]}|");
+    }
+}
+
+int[] ARR = new int[8];
+FillArray(ARR);
+PrintArray(ARR);
+Console.WriteLine();
+Console.WriteLine();
