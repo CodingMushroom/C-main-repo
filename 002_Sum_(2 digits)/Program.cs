f760ad48ac71648bc8d1cfe06589f373bb04e672
Victor <mail.baryshnikov@yandex.ru>
2022-10-30 20:52:21 +0400

    Formatting

--- 002_Sum_(2 digits)/Program.cs
+++ 002_Sum_(2 digits)/Program.cs
@@ -1,8 +1,19 @@
 ﻿// Задание: Вывести сумму 2-ух чисел
 
 int numberA = new Random().Next(1,10); // 0 1 2 3 4 ... 9
-Console.WriteLine(numberA);
+Console.WriteLine($"число A = {numberA}");
+
 int numberB = new Random().Next(1,10);
-Console.WriteLine(numberB);
+Console.WriteLine($"число B = {numberB}");
+
 int result = numberA + numberB;
-Console.WriteLine(numberA + numberB);
+Console.WriteLine($"A + B = {numberA + numberB}");
+
+// Вариант 2 - метод суммирования с рекурсией
+
+int SumRec(int N)
+{
+    if (N==0) return 0;
+    else return N + SumRec(N - 1);
+}
+Console.WriteLine($"Пример 2. Сумма чисел от 1 до 10 методом рекурсии = {SumRec(10)}");
---
+++ 060_String_from_A_to_B/Program.cs
@@ -0,0 +1,44 @@
+﻿// Упражнение из урока 7 Сборка строки чисел от меньшего к большему и наоборот
+
+// Вариант 1 Итеративный подход (от меньшего к большему)
+string NumbersFor1(int A, int B)
+{
+    string Result = string.Empty;
+    for (int i = A; i <= B; i++)
+    {
+        Result += $"{i} "; // Result = Result + i, тк это string, происходит запись строки
+    }
+    return Result;
+}
+Console.WriteLine(NumbersFor1(5,15));
+
+// Вариант 2 - рекурсия (от меньшего к большему)
+
+string NumbersRec1(int A, int B)
+{
+    if (A <= B) return $"{A} " + NumbersRec1(A+1, B);
+    else return String.Empty; // условие окончания процесса, чтобы издежать зацикливания
+}
+Console.WriteLine(NumbersRec1(5,15));
+
+// Вариант 3 Сборка строки (от большего к меньшему)
+
+string NumbersFor2(int A, int B)
+{
+    string Result = string.Empty;
+    for (int i = A; i >= B; i--)
+    {
+        Result += $"{i} ";
+    }
+    return Result;
+}
+Console.WriteLine(NumbersFor2(10,1));
+
+// Вариант 3 Сборка строки - рекурсия (от большего к меньшему)
+
+string NumbersRec2(int A, int B)
+{
+    if (A >= B) return $"{A} " + NumbersRec2(A - 1, B);
+    else return String.Empty; // условие окончания процесса, чтобы издежать зацикливания
+}
+Console.WriteLine(NumbersRec2(10,1));
