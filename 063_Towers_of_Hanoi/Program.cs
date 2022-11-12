f432a35c9be2a73fead8b7f25379bda12e26c4f9
Victor <mail.baryshnikov@yandex.ru>
2022-11-12 14:00:50 +0400

    Formatting

---
+++ 063_Towers_of_Hanoi/Program.cs
@@ -0,0 +1,10 @@
+﻿// // Урок 7 Пример: Игра "Ханойские башни"
+
+void Towers (string With = "1", string On = "3", string Some = "2",)
+{
+    if (Count > 1) Towers (With, Some, On, Count - 1);
+    Console.WriteLine($"{With}" >> {On}");
+    if (Count > 1) Towers (Some, On, With, Count - 1);
+}
+
+Towers();
