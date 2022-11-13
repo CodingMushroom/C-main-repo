544c8be4f5b90dd4d19519b5831dde8240b0af71
Victor <mail.baryshnikov@yandex.ru>
2022-11-13 16:16:26 +0400

    Formatting

--- 063_Towers_of_Hanoi/Program.cs
+++ 063_Towers_of_Hanoi/Program.cs
@@ -1,10 +1,15 @@
 ﻿// // Урок 7 Пример: Игра "Ханойские башни"
 
-void Towers (string With = "1", string On = "3", string Some = "2",)
+Console.Write("Введите сколько колец в игре - ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void Towers (string Start1, string Temp2, string End3, int Count) // With - откуда берем кольцо, On - куда кладем, Some - промежуточный шпиль
 {
-    if (Count > 1) Towers (With, Some, On, Count - 1);
-    Console.WriteLine($"{With}" >> {On}");
-    if (Count > 1) Towers (Some, On, With, Count - 1);
+    if (Count > 1) 
+        Towers (Start1, End3, Temp2, Count - 1); // перекладываем n-1 дисков с Start на End
+        Console.WriteLine ($"{Start1} >> {End3}"); // перекладываем 1 диск c Start на y
+    if (Count > 1) 
+        Towers (Temp2, Start1, End3, Count - 1); 
 }
 
-Towers();
+Towers("1", "2", "3", N);
