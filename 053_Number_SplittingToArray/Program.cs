8ac67bb17c6ca644788571693efcace521aa0a8f
Victor <mail.baryshnikov@yandex.ru>
2022-11-14 23:20:24 +0400

    Formatting

--- 053_Number_SplittingToArray/Program.cs
+++ 053_Number_SplittingToArray/Program.cs
@@ -5,7 +5,7 @@ int value = Convert.ToInt32(Console.ReadLine());
 
 value = Math.Abs(value);  // берем модуль числа, тк "-" тоже определиться в ячейку массива
 
-int size = value.ToString().Length;  // конвертация значения в строку
+int size = value.ToString().Length;  // записываем в int значение длины введенного числа
 int[] numbers_array = new int[size];
 
 while (value != 0)
--- 066_Positive_Digits_Printed/Program.cs
+++ 066_Positive_Digits_Printed/Program.cs
@@ -1,7 +1,28 @@
 ﻿// Определить сколько чисел больше 0 введено с клавиатуры (№42)
 
-System.Console.Write("Введите набор чисел через пробел или запятую: ");
-string Text = Convert.ToString(Console.ReadLine());
+System.Console.Write("Введите набор отрицательных или положительных чисел разделений (без пробелов, запятых, точек итд) =  ");
+string Numbers = Convert.ToString(Console.ReadLine());
+
+string[] Numbers_to_Text = new string[Numbers.Length]; // создаем строковый массив длиной в значение длины введенных чисел
+
+void ClearText(string A, string[] X)
+{
+    for (int i = 0; i < X.Length; i++)
+        int k = 0;
+    {
+        if (A[i] == ',') k++;
+        else if (A[i] == ' ') k++;
+        else if (A[i] == '.') k++;
+        else if (A[i] == '/') k++;
+        else X[k] = X[k] + $"{A[i]}"; // записываем значения (кроме " " "," "/" и ".") в строчку
+    }
+    k++;
+}
+
+void Converter
+{
+    
+}
 
 Text = T.Split(',');
 Text = T.Split(' ');
