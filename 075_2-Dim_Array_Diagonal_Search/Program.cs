061d270b636ca28b9a9145279d6d04c7761d4e5a
Victor <mail.baryshnikov@yandex.ru>
2022-12-04 20:44:04 +0400

    Formatting

---
+++ 075_2-Dim_Array_Diagonal_Search/Program.cs
@@ -0,0 +1,75 @@
+﻿// В матрице чисел найти сумму элементов главной диагонали (№54)
+
+// Методы
+
+void FillArray(int[,] Array)
+{
+    for (int m = 0; m < Array.GetLength(0); m++)
+    {
+        for (int n = 0; n < Array.GetLength(1); n++)
+        {
+            Array[m, n] = new Random().Next(1, 20);
+        }
+    }
+}
+
+void PrintArray(int[,] Array)
+{
+    for (int m = 0; m < Array.GetLength(0); m++)
+    {
+        for (int n = 0; n < Array.GetLength(1); n++)
+        {
+            Console.Write($"{Array[m, n]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int Diagonal_Sum(int[,] Array)
+{
+    int Sum = 0;
+    for (int m = 0; m < Array.GetLength(0); m++)
+    {
+        for (int n = 0; n < Array.GetLength(1); n++)
+        {
+             if (m == n) Sum += Array[m, n];
+        }
+    }
+    return Sum;
+}
+
+// Ввод и решение
+
+Console.WriteLine();
+Console.Write("Задайте высоту (M) и ширину (N) массива через запятую = ");
+string Text = Console.ReadLine();
+Console.WriteLine();
+
+string[] Number;
+if (Text.Contains(",")) Number = Text.Split(',');
+else Number = Text.Split('.');
+
+int.TryParse(Number[0], out int M);
+int.TryParse(Number[1], out int N);
+
+int[,] Array = new int[M, N];
+
+FillArray(Array);
+Console.WriteLine($"Сгенерирован 2-умерный массив с высотой {M} и шириной {N} = ");
+PrintArray(Array);
+Console.WriteLine();
+
+Console.WriteLine("Сумма элементов главной диагонали равна: " + Diagonal_Sum(Array));
+Console.WriteLine();
+
+
+// Альтернативный метод суммирования:
+//int Diagonal_Sum(int[,] Array)
+//{
+//    int Sum = 0;
+//   for (int m = 0; m < Array.GetLength(0); m++)
+//    {
+//        Sum += Array[m, m];
+//    }
+//    return Sum;
+//}
