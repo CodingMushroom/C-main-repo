092dffa1938b315fbe82ebe190bdb991267414ee
Victor <mail.baryshnikov@yandex.ru>
2022-12-04 15:58:45 +0400

    Formatting

---
+++ 070_2-Dimensional_Array_m-n_(part2)/Program.cs
@@ -0,0 +1,52 @@
+﻿// 49. Показать двумерный массив размером m×n заполненный вещественными числами
+
+
+// Методы
+void FillArray(double[,] Array)                                          
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Array[i, j] = new Random().NextDouble() * new Random().Next(1,20);    // по умолч ф-я NextDouble() - выдает диапазон от 0 до 1. Поэтому можно домножать значения на случ целые числа
+        }
+    }
+}
+
+void PrintArray(double[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Console.Write($"{Array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Ввод данных
+Console.WriteLine();
+Console.Write("Задайте высоту (m) и ширину (n) массива через запятую = ");
+string Text = Console.ReadLine();
+Console.WriteLine();
+
+string[] Number;
+if (Text.Contains(",")) Number = Text.Split(',');
+else Number = Text.Split('.');
+
+int.TryParse(Number[0], out int M);
+int.TryParse(Number[1], out int N);
+
+double[,] array = new double[M, N];
+
+FillArray(array);
+
+Console.Write($"Сгенерирован 2-умерный массив с высотой {M} и шириной {N} = ");
+Console.WriteLine();
+
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+
+
---
+++ 071_2-Dim_Array_Digits_Substitution/Program.cs
@@ -0,0 +1,65 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные (№50)
+
+
+// Методы
+void FillArray(int[,] Array)
+{
+    for (int n = 0; n < Array.GetLength(0); n++)
+    {
+        for (int k = 0; k < Array.GetLength(1); k++)
+        {
+            Array[n, k] = new Random().Next(1, 20);
+        }
+    }
+}
+
+void PrintArray(int[,] Array)
+{
+    for (int n = 0; n < Array.GetLength(0); n++)
+    {
+        for (int k = 0; k < Array.GetLength(1); k++)
+        {
+            Console.Write($"{Array[n, k]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeArray(int[,] Array)
+{
+    for (int n = 0; n < Array.GetLength(0); n++)
+    {
+        for (int k = 0; k < Array.GetLength(1); k++)
+        {
+            if (Array[n, k] % 2 == 0) Array[n, k] = -Array[n, k];
+        }
+    }
+}
+
+
+// Ввод данных
+Console.WriteLine();
+Console.Write("Задайте высоту (n) и ширину (k) массива через запятую = ");
+string Text = Console.ReadLine();
+Console.WriteLine();
+
+string[] Number;
+if (Text.Contains(",")) Number = Text.Split(',');
+else Number = Text.Split('.');
+
+int.TryParse(Number[0], out int N);
+int.TryParse(Number[1], out int K);
+
+int[,] Array = new int[N, K];
+
+FillArray(Array);
+
+Console.WriteLine($"Сгенерирован 2-умерный массив с высотой {N} и шириной {K} = ");
+PrintArray(Array);
+Console.WriteLine();
+
+ChangeArray(Array);
+Console.WriteLine("Массив с замененными элементами = ");
+PrintArray(Array);
+Console.WriteLine();
+
